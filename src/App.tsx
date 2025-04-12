ff9c715cc1d35d8f889a2b41a39ed1e12f6b4c40
mdrakib719 <mdrakibislam719@gmail.com>
2025-04-12 13:38:11 +0600

    last one

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/providers/ThemeProvider";
-
 import Index from "./pages/Index";
 import Projects from "./pages/Projects";
 import Blog from "./pages/Blog";
